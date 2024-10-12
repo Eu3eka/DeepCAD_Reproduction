1ce35fd4463796d05a080e953fce478911940411
Eu3eka <q2396979589@163.com>
2024-10-12 17:39:13 +0800

    sh

--- evaluation/run_eval_gen.sh
+++ evaluation/run_eval_gen.sh
@@ -1,5 +1,5 @@
 python collect_gen_pc.py --src $1
-python evaluate_gen_torch.py --src "${1}_pc" --n_test $3 -g $4
+python evaluate_gen_torch.py --src "${1}_pc" --n_test $2 -g $3
 rm -rf "${1}_pc"
 
 #Respectively:
